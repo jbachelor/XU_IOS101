c24a2d1fc09d4d1226ee5f6bb8c8fa18d9bac786
Jonathan Bachelor <jon_bachelor@me.com>
2017-07-28 07:50:31 -0700

    Completion of exercise 3. Added UI... No real functionality yet.

--- TipCalculator/MyViewController.cs
+++ TipCalculator/MyViewController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using CoreGraphics;
 using UIKit;
 
 namespace TipCalculator
@@ -7,6 +8,48 @@ namespace TipCalculator
     {
         public MyViewController()
         {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            View.BackgroundColor = UIColor.Yellow;
+            UITextField TotalAmount = CreateTotalAmountEntryField();
+            UIButton calcButton = CreateCalculateButton();
+            UILabel resultLabel = CreateResultLabel();
+
+            View.AddSubviews(TotalAmount, calcButton, resultLabel);
+        }
+
+        private UILabel CreateResultLabel()
+        {
+            UILabel resultLabel = new UILabel();
+            resultLabel.Frame = new CGRect(20, 124, View.Bounds.Width - 40, 40);
+            resultLabel.TextColor = UIColor.Blue;
+            resultLabel.TextAlignment = UITextAlignment.Center;
+            resultLabel.Font = UIFont.SystemFontOfSize(24);
+            resultLabel.Text = "Tip is $0.00";
+            return resultLabel;
+        }
+
+        private UIButton CreateCalculateButton()
+        {
+            UIButton calcButton = new UIButton(UIButtonType.Custom);
+            calcButton.Frame = new CGRect(20, 71, View.Bounds.Width - 40, 45);
+            calcButton.SetTitle("Calculate", UIControlState.Normal);
+            calcButton.BackgroundColor = UIColor.FromRGB(0f, 0.5f, 0f);
+            return calcButton;
+        }
+
+        private UITextField CreateTotalAmountEntryField()
+        {
+            UITextField TotalAmount = new UITextField();
+            TotalAmount.Frame = new CGRect(20, 28, View.Bounds.Width - 40, 35);
+            TotalAmount.KeyboardType = UIKeyboardType.DecimalPad;
+            TotalAmount.BorderStyle = UITextBorderStyle.RoundedRect;
+            TotalAmount.Placeholder = "Enter Total Amount";
+            return TotalAmount;
         }
     }
 }
